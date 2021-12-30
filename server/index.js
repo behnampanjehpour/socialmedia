122b8f81a506f227a43bf1000aa629efc430618b
Behnam Panjehpour <behnam@gmail.com>
2021-12-30 13:52:07 -0500

    save

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import router from './routes/posts.js'
+
+dotenv.config()
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+app.use('/posts', router)
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log('DB connected'))
+  .catch(err => {
+    console.log(err)
+  })
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server is running!')
+})
